526200a24f246134fd87af6c518aa338d3457a02
eplepakke <walawkizi@gmail.com>
2020-01-11 13:14:11 +0700

    Start window
    Feature for showing start window

---
+++ start_screen.py
@@ -0,0 +1,29 @@
+def start_screen():
+    intro_text = ["", "",
+                  "",
+                  "",
+                  ""]
+
+    fon = pygame.transform.scale(load_image('back.jpg'), (800, 600))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                return
+        pygame.display.flip()
+        clock.tick(FPS)
+       

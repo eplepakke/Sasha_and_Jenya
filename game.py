71399d2508e563d68c4a0abe4c544907c7d6c15e
keffffer <kefer04maya@gmail.com>
2020-01-16 19:18:09 +0700

    Feature for player
    feature for player

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import pygame
 import os
 from level_loading import load_level
 from start_screen import start_screen
+from player import Player
 
 WIN_WIDTH = 800
 WIN_HEIGHT = 640
@@ -12,7 +13,11 @@ FPS = 60
 PLATFORM_WIDTH = 32
 PLATFORM_HEIGHT = 32
 PLATFORM_COLOR = pygame.Color('red')
+entities = pygame.sprite.Group()
+platforms = []
 level = load_level('first.txt')
+hero = Player(50, 50)
+entities.add(hero)
 
 
 def main():
@@ -20,6 +25,13 @@ def main():
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode(SIZE)
     pygame.display.set_caption("Sasha and Jenya")
+
+    left = False
+    right = False
+    up = False
+    x = 0
+    y = 0
+
     bg = pygame.Surface((WIN_WIDTH, WIN_HEIGHT))
     bg.fill(BACKGROUND_COLOR)
     while True:
@@ -27,9 +39,24 @@ def main():
             if e.type == pygame.QUIT:
                 pygame.quit()
                 return
+            if e.type == pygame.KEYDOWN and e.key == pygame.K_LEFT:
+                left = True
+            if e.type == pygame.KEYDOWN and e.key == pygame.K_RIGHT:
+                right = True
+
+            if e.type == pygame.KEYUP and e.key == pygame.K_RIGHT:
+                right = False
+            if e.type == pygame.KEYUP and e.key == pygame.K_LEFT:
+                left = False
+
+            if e.type == pygame.KEYDOWN and e.key == pygame.K_UP:
+                up = True
+
+            if e.type == pygame.KEYUP and e.key == pygame.K_UP:
+                up = False
+
         screen.blit(bg, (0, 0))
-        x = 0
-        y = 0
+
         for row in level:
             for col in row:
                 if col == "#":
@@ -39,6 +66,10 @@ def main():
                 x += PLATFORM_WIDTH
             y += PLATFORM_HEIGHT
             x = 0
+
+        hero.update(left, right, up)
+        hero.draw(screen)
+
         pygame.display.update()
         clock.tick(FPS)
 
---
+++ player.py
@@ -0,0 +1,66 @@
+import pygame
+
+MOVE_SPEED = 7
+WIDTH = 22
+HEIGHT = 32
+COLOR = pygame.Color('green')
+JUMP_POWER = 10
+GRAVITY = 0.35
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.x_val = 0
+        self.y_val = 0
+        self.startX = x
+        self.startY = y
+        self.on_ground = False
+        self.image = pygame.Surface((WIDTH, HEIGHT))
+        self.image.fill(COLOR)
+        self.rect = pygame.Rect(x, y, WIDTH, HEIGHT)
+
+    def update(self, left, right, up, platforms):
+        if left:
+            self.x_val = -MOVE_SPEED
+
+        if right:
+            self.x_val = MOVE_SPEED
+
+        if not (left or right):
+            self.x_val = 0
+
+        if up:
+            if self.on_ground:
+                self.y_val = -JUMP_POWER
+
+        if not self.on_ground:
+            self.y_val += GRAVITY
+
+        self.on_ground = False
+        self.rect.y += self.y_val
+        self.collide(0, self.y_val, platforms)
+
+        self.rect.x += self.x_val
+        self.collide(self.x_val, 0, platforms)
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+    def collide(self, x_val, y_val, platforms):
+        for p in platforms:
+            if pygame.sprite.collide_rect(self, p):
+                if x_val > 0:
+                    self.rect.right = p.rect.left
+
+                if x_val < 0:
+                    self.rect.left = p.rect.right
+
+                if y_val > 0:
+                    self.rect.bottom = p.rect.top
+                    self.on_ground = True
+                    self.y_val = 0
+
+                if y_val < 0:
+                    self.rect.top = p.rect.bottom
+                    self.y_val = 0

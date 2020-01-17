411ae255dac903126fb7cc92339da6a943994f96
keffffer <kefer04maya@gmail.com>
2020-01-17 18:50:16 +0700

    save fixes for next changes

--- game.py
+++ game.py
@@ -1,8 +1,9 @@
-import pygame
-import os
+from pygame import *
 from level_loading import load_level
 from start_screen import start_screen
-from player import Player
+from player import *
+from platform import *
+from camera import *
 
 WIN_WIDTH = 800
 WIN_HEIGHT = 640
@@ -10,14 +11,24 @@ BACKGROUND_COLOR = pygame.Color('blue')
 SIZE = (WIN_WIDTH, WIN_HEIGHT)
 FPS = 60
 
-PLATFORM_WIDTH = 32
-PLATFORM_HEIGHT = 32
-PLATFORM_COLOR = pygame.Color('red')
-entities = pygame.sprite.Group()
+sprites = pygame.sprite.Group()
 platforms = []
 level = load_level('first.txt')
 hero = Player(50, 50)
-entities.add(hero)
+sprites.add(hero)
+
+
+def camera_configure(camera, target_rect):
+    l, t, _, _ = target_rect
+    _, _, w, h = camera
+    l, t = -l + WIN_WIDTH / 2, -t + WIN_HEIGHT / 2
+
+    l = min(0, l)
+    l = max(-(camera.width - WIN_WIDTH), l)
+    t = max(-(camera.height - WIN_HEIGHT), t)
+    t = min(0, t)
+
+    return pygame.Rect(l, t, w, h)
 
 
 def main():
@@ -25,6 +36,11 @@ def main():
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode(SIZE)
     pygame.display.set_caption("Sasha and Jenya")
+
+    total_level_width = len(level[0]) * PLATFORM_WIDTH
+    total_level_height = len(level) * PLATFORM_HEIGHT
+
+    camera = Camera(camera_configure, total_level_width, total_level_height)
 
     left = False
     right = False
@@ -34,6 +50,17 @@ def main():
 
     bg = pygame.Surface((WIN_WIDTH, WIN_HEIGHT))
     bg.fill(BACKGROUND_COLOR)
+
+    for row in level:
+        for col in row:
+            if col == "#":
+                pf = Platform(x, y)
+                sprites.add(pf)
+                platforms.append(pf)
+            x += PLATFORM_WIDTH
+        y += PLATFORM_HEIGHT
+        x = 0
+
     while True:
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
@@ -57,18 +84,11 @@ def main():
 
         screen.blit(bg, (0, 0))
 
-        for row in level:
-            for col in row:
-                if col == "#":
-                    pf = pygame.Surface((PLATFORM_WIDTH, PLATFORM_HEIGHT))
-                    pf.fill(PLATFORM_COLOR)
-                    screen.blit(pf, (x, y))
-                x += PLATFORM_WIDTH
-            y += PLATFORM_HEIGHT
-            x = 0
+        hero.update(left, right, up, platforms)
+        camera.update(hero)
 
-        hero.update(left, right, up)
-        hero.draw(screen)
+        for s in sprites:
+            screen.blit(s.image, camera.apply(s))
 
         pygame.display.update()
         clock.tick(FPS)
